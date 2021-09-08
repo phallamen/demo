8e70ece213d648ca463ca3163017f372b224ffc8
men.phalla <men.phalla@ababank.com>
2021-09-08 18:52:10 +0700

    add code

---
+++ src/main/java/com/devsecops/demodevsecops/DemoDevsecopsApplication.java
@@ -0,0 +1,13 @@
+package com.devsecops.demodevsecops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDevsecopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDevsecopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsecops/demodevsecops/DemoDevsecopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsecops.demodevsecops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDevsecopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
